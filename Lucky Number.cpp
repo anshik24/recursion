09cbe3162c151062e229fed26168b55f254d8a8e
Anshika Singh <99993107+anshik24@users.noreply.github.com>
2022-07-17 20:26:05 +0530

    Lucky Number

---
+++ Lucky Number.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User-function template for C++
+
+// Complete the function
+// n: Input n
+// Return True if the given number is a lucky number else return False
+
+class Solution{
+public:
+bool func(int n,int m)
+{
+    if(n%m==0) return false;
+    if(m>n) return true;
+    return func(n-(n/m),m+1);
+}
+    bool isLucky(int n) {
+    return func(n,2);
+    
+    
+    }
+};
+
+//{ Driver Code Starts.
+signed main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int n;
+        cin>>n;
+        Solution obj;
+        //calling isLucky() function
+        if(obj.isLucky(n))
+            cout<<"1\n";//printing "1" if isLucky() returns true
+        else
+            cout<<"0\n";//printing "0" if isLucky() returns false
+    }
+    
+}
+// } Driver Code Ends
